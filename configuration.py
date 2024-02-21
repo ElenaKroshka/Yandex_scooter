af3b2f872e38c353d76611a14e4c084796e95999
ElenaKroshka <153067488+ElenaKroshka@users.noreply.github.com>
2024-02-21 18:28:32 +0600

    Feature for Yandex scooter
    Yandex scooter autotest

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://b8f3a062-01ec-49ab-aba3-9d2aaa784e85.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://a8361994-b57e-48f6-b0b6-98c1e1904831.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDER_BY_TRACK = "/api/v1/orders/track"
